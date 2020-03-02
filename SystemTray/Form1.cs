4a4b8577e0e988e8867c0de83dea715221fbec48
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-03-02 20:24:12 +0200

    SystemTray (SystemTray files were added)

---
+++ SystemTray/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SystemTray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            createIconMenuStructure();
+            contextMenu1 = notifyIcon1.ContextMenu;
+        }
+
+        private void createIconMenuStructure()
+        {
+            contextMenu1.MenuItems.Add("Показать", new EventHandler(menuItem1_Click));
+            contextMenu1.MenuItems.Add("Скрыть", new EventHandler(menuItem2_Click));
+            notifyIcon1.Visible = true;
+            notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenu = contextMenu1;
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            contextMenu1.MenuItems[0].Enabled = false;
+            contextMenu1.MenuItems[1].Enabled = true;
+            this.Show();
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            contextMenu1.MenuItems[1].Enabled = false;
+            contextMenu1.MenuItems[0].Enabled = true;
+            this.Hide();
+        }
+
+    }
+}
