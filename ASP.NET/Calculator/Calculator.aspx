f8ae39d45e131e1fea3cd4564b163eb2ece30370
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-03-29 17:15:00 +0300

    Calculator (Calculator files were added)

---
+++ ASP.NET/Calculator/Calculator.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Calculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #Select1 {
+            width: 60px;
+            height: 37px;
+        }
+        #operation {
+            width: 65px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Hello! This is a simple calculator. </p>
+        </div>
+        <p>Number 1</p>
+        <p>
+            <asp:TextBox ID="num1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;<asp:DropDownList ID="sign" runat="server">
+                <asp:ListItem Selected="True" Value="+"> + </asp:ListItem>
+                  <asp:ListItem Value="-"> - </asp:ListItem>
+                  <asp:ListItem Value="*"> * </asp:ListItem>
+                  <asp:ListItem Value="/"> / </asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <p> Number 2</p>
+        <p>
+            <asp:TextBox ID="num2" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="solve" runat="server" Height="53px" Text="=" Width="115px" OnClick="solve_Click" />
+        <p><asp:TextBox ID="answer" runat="server" IsReadOnly="True" ReadOnly="True"  Width="300px"></asp:TextBox></p>
+    </form>      
+</body>
+</html>
---
+++ ASP.NET/Calculator/Calculator.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : Page
+    {
+        double number1, number2;
+        string operat = null;
+
+        static string pattern = @"^\D|\D$|.\D.";
+        static string patterncomma = @"[.,]";
+        Regex regex = new Regex(pattern);
+        Regex regexcomma = new Regex(patterncomma);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        protected void solve_Click(object sender, EventArgs e)
+        {
+            if (!regex.IsMatch(num1.Text) || regexcomma.IsMatch(num1.Text))
+            {
+                Double.TryParse(num1.Text, out number1);
+                
+            } else
+            {
+                answer.Text = "Error. Entered number had a mistake";
+                return;
+            }
+
+            if (!regex.IsMatch(num2.Text) || regexcomma.IsMatch(num2.Text))
+            {
+                Double.TryParse(num2.Text, out number2);
+            }
+            else
+            {
+                answer.Text = "Error. Entered number had a mistake";
+                return;
+            }
+
+            operat = sign.SelectedItem.Value;
+
+            switch (operat)
+            {
+                case "+":
+                    answer.Text = (number1 + number2).ToString();
+                    break;
+                case "-":
+                    answer.Text = (number1 - number2).ToString();
+                    break;
+                case "*":
+                    answer.Text = (number1 * number2).ToString();
+                    break;
+                case "/":
+                    answer.Text = (number1 / number2).ToString();
+                    break;
+            }
+        }
+    }
+}
