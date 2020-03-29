1db9cf6d34db64c15b33d68f671e7be6cdc927cb
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-03-29 17:46:19 +0300

    LogInForm (LogInForm files were added)

---
+++ ASP.NET/LogInForm/Log in.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Log in.aspx.cs" Inherits="LogIn.Log_in" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Hello. It is a simple log in form<p>
+            Name (max 10)</p>
+        <asp:TextBox ID="name" runat="server" Width="201px"></asp:TextBox>
+        <p>
+            E-mail</p>
+        <asp:TextBox ID="email" runat="server" Width="202px"></asp:TextBox>
+        <p>
+            <asp:Button ID="login" runat="server" Height="59px" OnClick="Button1_Click" Text="Log in" Width="143px" />
+        </p>
+        <asp:TextBox ID="result" runat="server" Height="44px" ReadOnly="True" Width="397px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/LogInForm/Log in.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LogIn
+{
+    public partial class Log_in : Page
+    {
+        static string patternName = @"\D{1,10}";
+        Regex regexName = new Regex(patternName);
+
+        
+        static string patternEmail = @"^(([^<>()[\]\\.,;:\s@\""]+(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$";
+        Regex regexEmail = new Regex(patternEmail);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (regexName.IsMatch(name.Text) && regexEmail.IsMatch(email.Text))
+            {
+                result.Text = "You have successfully loged in";
+            }
+            else
+            {
+                result.Text = "Entered data had a mistake. Please re-fill out the form";
+            }
+        }
+    }
+}
