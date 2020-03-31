11edb146cee4e59e7a98b25d3e804940da618714
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-03-31 23:15:21 +0300

    BrowserName (BrowserName files were added)

---
+++ ASP.NET/BrowserName/BrowserName.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BrowserName.aspx.cs" Inherits="BrowserName.BrowserName" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+        </div>
+        <asp:TextBox ID="browser" runat="server" Height="46px" ReadOnly="True" Width="568px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/BrowserName/BrowserName.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BrowserName
+{
+    public partial class BrowserName : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var userAgent = HttpContext.Current.Request.UserAgent;
+            var userBrowser = new HttpBrowserCapabilities { Capabilities = new Hashtable { { string.Empty, userAgent } } };
+            var factory = new BrowserCapabilitiesFactory();
+            factory.ConfigureBrowserCapabilities(new NameValueCollection(), userBrowser);
+            browser.Text = "Your browser is " + userBrowser.Browser.ToString();       
+        }
+    }
+}
