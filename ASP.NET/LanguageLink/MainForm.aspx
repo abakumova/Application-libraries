6ddd9f4617918a6d267516e2e90fa8e8fe8038bb
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-02 21:02:54 +0300

    LanguageLink (LanguageLink files were added)

---
+++ ASP.NET/LanguageLink/MainForm.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="LanguageLink.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="page" runat="server" Height="46px" ReadOnly="True" Width="688px"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="lang" runat="server" Height="49px" ReadOnly="True" Width="688px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/LanguageLink/MainForm.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LanguageLink
+{
+    public partial class MainForm : Page 
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String s = CultureInfo.CurrentCulture.Name;
+
+            if (HttpContext.Current.Request.UrlReferrer == null )
+            {
+                if (s.Equals("ru-RU"))
+                {
+                    page.Text = "Вы не посещали страницы";
+                    lang.Text = s;
+                }
+                if (s.Equals("en-EN"))
+                {
+                    page.Text = "You haven't visited pages yet";
+                    lang.Text = s;
+                }
+            }
+            else
+            {
+                page.Text = ConvertTextUrlToLink(Page.PreviousPage.ToString());
+            }
+            
+        }
+        private string ConvertTextUrlToLink(string url)
+        {
+            string regex = @"((www\.|(http|https|ftp|news|file)+\:\/\/)[_.a-z0-9-]+\.[a-z0-9\/_:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])";
+            Regex r = new Regex(regex, RegexOptions.IgnoreCase);
+            return r.Replace(url, "a href=\"$1\" title=\"Click here to open in a new window or tab\"target =\"_blank\">$1</a>").Replace("href=\"www", "href=\"http://www");
+        }
+    }
+}
