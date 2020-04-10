13f6360c432fcc9e97c74ea9b2427ec20e13d97f
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-10 21:25:39 +0300

    IncreasingSequence (IncreasingSequence files were added)

---
+++ ASP.NET/IncreasingSequence/MainForm.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IncreasingSequence
+{
+    public partial class MainForm : Page
+    {
+        List<double> sequence = new List<double>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+               
+        }
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double k;
+            string[] numbers = TextBox1.Text.Split(' ');
+            foreach (var item in numbers)
+            {
+                if (double.TryParse(item, out k))
+                {
+                    sequence.Add(k);
+                }
+            }
+            TextBox1.Text = "";
+
+            sequence.Sort();
+            string value = "";
+            foreach (var i in sequence)
+            {
+                value += (i + " ");
+            }
+            TextBox2.Text = value;
+        }
+    }
+}
