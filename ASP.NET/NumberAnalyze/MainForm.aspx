873279b8a56afc535bd1fdf58d04739035af7590
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-11 21:33:54 +0300

    NumberAnalyze (NumberAnalyze files were added)

---
+++ ASP.NET/NumberAnalyze/MainForm.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="AnalyzeNumber.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Enter a number"></asp:Label>
+            <asp:TextBox ID="number" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Analyze" OnClick="Button1_Click" />
+        </div>
+        <asp:TextBox ID="result" runat="server" ReadOnly="True" Height="222px" Width="347px" TextMode="MultiLine"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/NumberAnalyze/MainForm.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AnalyzeNumber
+{
+    public partial class MainForm : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double num;
+
+           if(Double.TryParse(number.Text,out num))
+           {
+                result.Text = "Entered value " + num;
+
+                if (num % 2 == 0)
+                {
+                    result.Text += "\nEvenness of number: even";
+                }
+                else
+                {
+                    result.Text += "\nEvenness of number: odd";
+                }
+
+                if (num < 0)
+                {
+                    result.Text += "\nSign: - ";
+                }
+                if (num > 0)
+                {
+                    result.Text += "\nNumber symbol: + ";
+                }
+
+                result.Text += "\nNumber of digits: " + Math.Abs(num).ToString().Length;
+            }
+           else
+           {
+                result.Text = "\n It's not a number";
+           }
+        }
+    }
+}
