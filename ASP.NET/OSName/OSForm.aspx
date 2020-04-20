874fde236321c2641cd562525d681cd8c084dc09
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-20 14:41:31 +0300

    OSName (OSName files were added)

---
+++ ASP.NET/OSName/OSForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OSForm.aspx.cs" Inherits="OSName.OSForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="osname" runat="server" Height="85px" ReadOnly="True" Width="610px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/OSName/OSForm.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OSName
+{
+    public partial class OSForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            osname.Text = "Your OS is " + osNameAndVersion.ToString();
+        }
+    }
+}
