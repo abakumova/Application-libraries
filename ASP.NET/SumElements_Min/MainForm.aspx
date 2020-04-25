e1cf5f1613d8c177a955155010e0f11da0a617e8
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-25 22:19:30 +0300

    SumElements_Min (SumElements_Min files were added)

---
+++ ASP.NET/SumElements_Min/MainForm.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="IncreasingSequence.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script> </script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Enter numbers using spaces
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server" Width="753px"></asp:TextBox>
+        </p>
+        <asp:Button type="button" ID="Button2" runat="server" OnClick="Button2_Click" Text="Display sequence" />
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server" Height="44px" ReadOnly="True" Width="759px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label1" runat="server" Text="Sum of elements"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" ReadOnly="True"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Minimum"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server" ReadOnly="True"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/SumElements_Min/MainForm.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IncreasingSequence
+{
+    public partial class MainForm : Page
+    {
+        List<double> sequence = new List<double>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+               
+        }
+ 
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double k;
+            string[] numbers = TextBox1.Text.Split(' ');
+            foreach (var item in numbers)
+            {
+                if (double.TryParse(item, out k))
+                {
+                    sequence.Add(k);
+                }
+            }
+            TextBox1.Text = "";
+            sequence.Sort();
+            sequence.Reverse();
+            string value = "";
+            double d_value = 0;
+            foreach (var i in sequence)
+            {
+                value += (i + " ");
+                d_value += i;
+            }
+            TextBox2.Text = value;
+            TextBox3.Text = (d_value).ToString();
+            TextBox4.Text = (sequence.Min()).ToString();
+        }
+    }
+}
